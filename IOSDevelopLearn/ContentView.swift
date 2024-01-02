b819fcf09854c274768fa17317a9895183b036fe
SEKI-YUTA <zard.since1991@gmail.com>
2024-01-03 00:27:57 +0900

    Initial Commit

---
+++ IOSDevelopLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSDevelopLearn
+//
+//  Created by 関佑太 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSDevelopLearn/IOSDevelopLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSDevelopLearnApp.swift
+//  IOSDevelopLearn
+//
+//  Created by 関佑太 on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct IOSDevelopLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
